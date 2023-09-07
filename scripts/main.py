3d78c6fb7dc541ebeada00aba02410753107509c
WillyLutz <lutz0willy@gmail.com>
2023-09-07 10:20:24 +0200

    emergency push

--- scripts/main.py
+++ scripts/main.py
@@ -33,17 +33,107 @@ from scipy.signal import find_peaks
 
 
 def main():
-    timepoint = "T=48H"
-    min_freq = 0
-    max_freq = 5000
-    percentiles = 0.1
-    batches = {"batch 1": ["1", "2", "3"], "batch 2": ["4", "5", "6", ], "batch 3": ["7", "8", "9", ],
-               "batch 4": ["10", "11", "12"],
-               "batch 1_2": ["1", "2", "3", "4", "5", "6", ], "batch 1_4": ["1", "2", "3", "10", "11", "12", ],
-               "batch 2_4": ["4", "5", "6", "10", "11", "12"],
-               "all slices": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]}
+    class1_df = fp.make_dataset_from_freq_files(parent_dir=P.NODRUG,
+                                                to_include=("odd_harmonics", timepoint),
+                                                to_exclude=("TTX", "RG27", "flattened", "TAHV"),
+                                                verbose=False,
+                                                save=False,
+                                                freq_range=(min_freq, max_freq),
+                                                select_samples=batches["batch 4"],
+                                                separate_samples=False,
+                                                label_comment=f"",
+                                                target_keys={'NI': 'Mock',
+                                                             'TAHV': 'TAHV'})  # IMPORTANT, else it may result in an empty dataframe
+    discarded_class1 = fp.discard_outliers_by_iqr(class1_df, low_percentile=percentiles,
+                                                  high_percentile=1 - percentiles,
+                                                  mode='capping')
 
-    cp.Confusion_matrix(0, 5000, "batch 1", "T=48H") # Exemple of use
+    percentiles = 0.1
+    base = "/home/wlutz/PycharmProjects/tahynavirus-electrical-analysis/datasets"
+    t0mock = pd.read_csv(f'{base}/DATASET_T=0 MOCK.csv')
+    dt0mock = fp.discard_outliers_by_iqr(t0mock, low_percentile=percentiles, high_percentile=1 - percentiles,
+                                         mode='capping')
+    dt0mock['label'].replace('Mock', 'Mock\n0min', inplace=True)
+
+    t0vlpp = pd.read_csv(f'{base}/DATASET_T=0 VLP+.csv')
+    dt0vlpp = fp.discard_outliers_by_iqr(t0vlpp, low_percentile=percentiles, high_percentile=1 - percentiles,
+                                         mode='capping')
+    dt0vlpp['label'].replace('VLP+', 'VLP+\n0min', inplace=True)
+
+    t24mock = pd.read_csv(f'{base}/DATASET_T=24H MOCK.csv')
+    dt24mock = fp.discard_outliers_by_iqr(t24mock, low_percentile=percentiles, high_percentile=1 - percentiles,
+                                          mode='capping')
+    dt24mock['label'].replace('Mock', 'Mock\n24h', inplace=True)
+
+    t0spike = pd.read_csv(f'{base}/DATASET_T=0 SPIKE.csv')
+    dt0spike = fp.discard_outliers_by_iqr(t0spike, low_percentile=percentiles, high_percentile=1 - percentiles,
+                                          mode='capping')
+    dt0spike['label'].replace('Spike', 'Spike\n0min', inplace=True)
+
+    t0inf = pd.read_csv(f'{base}/DATASET_T=0 INF.csv')
+    dt0inf = fp.discard_outliers_by_iqr(t0inf, low_percentile=percentiles, high_percentile=1 - percentiles,
+                                        mode='capping')
+    dt0inf['label'].replace('Sars-CoV', 'Sars-CoV\n0min', inplace=True)
+
+    t24inf = pd.read_csv(f'{base}/DATASET_T=24H INF.csv')
+    dt24inf = fp.discard_outliers_by_iqr(t24inf, low_percentile=percentiles, high_percentile=1 - percentiles,
+                                         mode='capping')
+    dt24inf['label'].replace('Sars-CoV', 'Sars-CoV\n24h', inplace=True)
+
+    rfc, _ = fl.train_RFC_from_dataset(pd.concat([dt0mock,
+                                                  dt0inf,
+                                                  dt0vlpp,
+                                                  dt0spike,
+                                                  dt24mock,
+                                                  dt24inf], ignore_index=True), )
+
+    inf = pd.read_csv(f'{base}/DATASET_T=24H INF.csv')
+    dinf = fp.discard_outliers_by_iqr(inf, low_percentile=percentiles, high_percentile=1 - percentiles,
+                                      mode='capping')
+    dinf['label'].replace('Sars-CoV', 'Sars-CoV\n24h', inplace=True)
+
+    vlpp = pd.read_csv(f'{base}/DATASET_T=24H VLP+.csv')
+    dvlpp = fp.discard_outliers_by_iqr(vlpp, low_percentile=percentiles, high_percentile=1 - percentiles,
+                                       mode='capping')
+    dvlpp['label'].replace('VLP+', 'VLP+\n24h', inplace=True)
+
+    spike = pd.read_csv(f'{base}/DATASET_T=24H SPIKE.csv')
+    dspike = fp.discard_outliers_by_iqr(spike, low_percentile=percentiles, high_percentile=1 - percentiles,
+                                        mode='capping')
+    dspike['label'].replace('Spike', 'Spike\n24h', inplace=True)
+
+    testdf = pd.concat([dvlpp, dinf, dspike, ], ignore_index=True)
+
+    # globaldf = pd.concat([traindf, testdf], ignore_index=True)
+    globaldf = pd.concat([dt0mock,
+                          dt0inf,
+                          dt0vlpp,
+                          dt0spike,
+                          dt24mock,
+                          dinf,
+                          dspike,
+                          dvlpp, ], ignore_index=True)
+
+    fl.test_rfc_by_confusion(rfc, globaldf, training_targets=('Mock\n0min',
+                                                              'Sars-CoV\n0min',
+                                                              'VLP+\n0min',
+                                                              'Spike\n0min',
+                                                              'Mock\n24h',
+                                                              'Sars-CoV\n24h'),
+                             testing_targets=tuple(set(list(('Mock\n0min',
+                                                             'Sars-CoV\n0min',
+                                                             'VLP+\n0min',
+                                                             'Spike\n0min',
+                                                             'Mock\n24h',
+                                                             'Sars-CoV\n24h',
+                                                             'VLP+\n24h',
+                                                             'Spike\n24h')))),
+                             show=True, verbose=False, savepath='',
+                             title=f"",
+                             iterations=5, )
+
+    # training(mock0, vlp-0, vlp+0, spike0, inf0, mock24 as MOCK and inf24 as INF) testing()
+
 
 now = datetime.datetime.now()
 print(now)
